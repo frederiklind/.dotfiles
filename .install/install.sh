bcbb6f761c40e278532c133d1276755aab32281c
Frederik Lind <frederik.lind.mail@gmail.com>
2024-09-24 09:25:50 +0200

    install script wip

--- .install/install.sh
+++ .install/install.sh
@@ -28,10 +28,8 @@ MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 BOLD='\033[1m'
 NONE='\033[0m'
-
 CONFIG="${HOME}/.config"
 DOTFILES="${HOME}/.dotfiles"
-
 INSTALL_LOG="${HOME}/.dotfiles/.install/install.log"
 
 # =============================================================================
@@ -59,10 +57,10 @@ function mk_directories() {
 }
 
 # =============================================================================
-# ------------------------------- Window Manager -------------------------------
+# ------------------------------- Window Manager ------------------------------
 # =============================================================================
 
-function stuff() {
+function window_manager() {
     echo 
     echo -e "${CYAN}${BOLD}> ${BLUE}${BOLD}Window Manager${NONE}"
     echo "|"
@@ -74,17 +72,133 @@ function stuff() {
     echo "|"
     case "$choice" in
         1)
+            local hyprpkgs=(
+                "hyprland"    \
+                "hyprlock"    \ 
+                "waybar"      \ 
+                "wofi"
+            )
             echo -e "|-${CYAN}${BOLD}> ${NONE}Installing ${BLUE}${BOLD}hyprland${NONE}"
+            for pkg in "${hyprpkgs[@]}"; do
+                install_package "$pkg"
+            done
+
+            if [[ -d "${CONFIG}/hypr" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}hypr${NONE} configuration"
+                mv "${CONFIG}/hypr" "${DOTFILES}/.backup/hypr"
+            fi
+
+            mkdir "${CONFIG}/hypr"
+
+            ln -s "${DOTFILES}/hypr/.conf/default.conf" "${CONFIG}/hypr/hyprland.conf"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/.conf/default.conf -> ${CYAN}~/.config/hypr/hyprland.conf${NONE}"
+            
+            ln -s "${DOTFILES}/hypr/hyprlock.conf" "${CONFIG}/hypr/hyprlock.conf"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/hypr/hyprlock.conf -> ${CYAN}~/.config/hypr/hyprlock.conf${NONE}"
+
+            if [[ -d "${CONFIG}/waybar" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}waybar${NONE} configuration"
+                mv "${CONFIG}/waybar" "${DOTFILES}/.backup/waybar"
+            fi
+
+            ln -s "${DOTFILES}/hypr/waybar/themes/theme-1" "${CONFIG}/waybar"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/hypr/waybar/themes/theme-1 -> ${CYAN}~/.config/waybar${NONE}"
             ;;
         2)
+            local bspwmpkgs=(
+                "bspwm"         \
+                "sxhkd"         \
+                "polybar"       \
+                "rofi"          \
+                "picom"         \
+                "dunst"         \
+                "feh"           \
+                "xorg-xinit"    \
+                "xorg-server"   \
+                "xorg-xrandr"   \
+                "xorg-xset"     \
+                "xorg-xsetroot"
+            )
             echo -e "|-${CYAN}${BOLD}> ${NONE}Installing ${BLUE}${BOLD}bspwm${NONE}"
-            # install_package "bspwm"
+            for pkg in "${bspwmpkgs[@]}"; do
+                install_package "$pkg"
+            done
+
+            if [[ -d "${CONFIG}/bspwm" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}bspwm${NONE} configuration"
+                mv "${CONFIG}/bspwm" "${DOTFILES}/.backup/bspwm"
+            fi
+
+            ln -s "${DOTFILES}/bspwm" "${CONFIG}/bspwm"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/bspwm -> ${CYAN}~/.config/bspwm${NONE}"
+
+            if [[ -d "${CONFIG}/sxhkd" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}sxhkd${NONE} configuration"
+                mv "${CONFIG}/sxhkd" "${DOTFILES}/.backup/sxhkd"
+            fi
+
+            ln -s "${DOTFILES}/sxhkd" "${CONFIG}/sxhkd"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/sxhkd -> ${CYAN}~/.config/sxhkd${NONE}"
+
+            if [[ -d "${CONFIG}/polybar" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}polybar${NONE} configuration"
+                mv "${CONFIG}/polybar" "${DOTFILES}/.backup/polybar"
+            fi
+
+            ln -s "${DOTFILES}/polybar" "${CONFIG}/polybar"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/polybar -> ${CYAN}~/.config/polybar${NONE}"
+
+            if [[ -f "${HOME}/.Xresources" ]]; then
+                echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}.Xresources${NONE}"
+                mv "${HOME}/.Xresources" "${DOTFILES}/.backup/.Xresources"
+            fi
+
+            ln -s "${DOTFILES}/.Xresources" "${HOME}/.Xresources"
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ~/.dotfiles/.Xresources -> ${CYAN}~/.Xresources${NONE}"
             ;;
         *)
             echo -e "|-${RED}${BOLD}> ${NONE}Invalid option"
             ;;
     esac
 }
+
+# =============================================================================
+# --------------------------------- AUR-Helper  -------------------------------
+# =============================================================================
+
+function aur_helper() {
+    echo
+    echo -e "${CYAN}${BOLD}> ${BLUE}${BOLD}AUR Helper${NONE}"
+    echo "|"
+    echo -e "|-${YELLOW}${BOLD}> ${BLUE}Available Options:${NONE}"
+    echo -e "|--${CYAN}${BOLD} 1. ${GREEN}yay${NONE}"
+    echo -e "|--${CYAN}${BOLD} 2. ${GREEN}paru${NONE}"
+    echo "|"
+    read -p "|--? Choose an AUR helper: " choice
+    echo "|"
+    sudo pacman -S --needed base-devel --noconfirm &>> $INSTALL_LOG
+    cd "${HOME}/Source/aur"
+    case "$choice" in
+        1)
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Cloning ${BLUE}${BOLD}yay${NONE}"
+            git clone https://aur.archlinux.org/yay.git &>> $INSTALL_LOG
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Building ${BLUE}${BOLD}yay${NONE} package"
+            cd yay && makepkg -si &>> $INSTALL_LOG
+            ;;
+        2)
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Cloning ${BLUE}${BOLD}paru${NONE}"
+            git clone https://aur.archlinux.org/paru.git &>> $INSTALL_LOG
+            echo -e "|-${CYAN}${BOLD}> ${NONE}Building ${BLUE}${BOLD}paru${NONE} package"
+            cd paru && makepkg -si &>> $INSTALL_LOG
+            ;;
+        *)
+            echo -e "|-${RED}${BOLD}> ${NONE}Invalid option"
+            aur_helper
+            ;;
+    esac
+    cd "${HOME}"
+}
+
 
 # =============================================================================
 # ------------------------------- Languages -----------------------------------
@@ -116,6 +230,10 @@ function langs() {
     for package in "${packages[@]}"; do
        install_package "$package" 
     done
+    
+    if [[ ! -d "${HOME}/.venvs" ]]; then
+        mkdir "${HOME}/.venvs"
+    fi
 }
 
 # =============================================================================
@@ -141,22 +259,32 @@ function dev_tools() {
     if [[ "${choice}" == "y" || "${choice}" == "Y" ]]; then
         install_package "docker"    
     fi
+
+    # neovim stuff
+    local nvimpkgs=(
+        "mercurial"     \
+        "python-pynvim" \
+        "luarocks"      \
+
+    )
+
+    sudo npm install -g neovim &>> $INSTALL_LOG
     
     echo "|"
     echo -e "|-${CYAN}${BOLD}> ${BLUE}${BOLD}Setting up symlinks${NONE}"
 
     if [[ -d "${CONFIG}/nvim" ]]; then
         echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}nvim${NONE}"
-        # mv "${CONFIG}/nvim" "${DOTFILES}/.backup/nvim"
+        mv "${CONFIG}/nvim" "${DOTFILES}/.backup/nvim"
     fi
-    # ln -s "${DOTFILES}/nvim" "${CONFIG}/nvim"
+    ln -s "${DOTFILES}/nvim" "${CONFIG}/nvim"
     echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ${BLUE}${BOLD}nvim${NONE}"
 
     if [[ -f "${HOME}/.tmux.conf" ]]; then
         echo -e "|-${CYAN}${BOLD}> ${NONE}Backing up existing ${BLUE}${BOLD}tmux${NONE}"
-        # mv "${HOME}/.tmux.conf" "${DOTFILES}/.backup/tmux.conf"
+        mv "${HOME}/.tmux.conf" "${DOTFILES}/.backup/tmux.conf"
     fi
-    # ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
+    ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
     echo -e "|-${CYAN}${BOLD}> ${NONE}Symlinked ${BLUE}${BOLD}tmux${NONE}"
 
     if [[ ! -f "${CONFIG}/pycodestyle" ]]; then
@@ -206,7 +334,8 @@ function install() {
     touch "${INSTALL_LOG}"
     mkdir "${DOTFILES}/.backup"
     # mk_directories
-    stuff
+    window_manager
+    aur_helper
     langs
     dev_tools
     setup_firewall
@@ -232,15 +361,15 @@ Arch linux / hyprland WM installation.
 EOF
 echo -e "${NONE}"
 
-# echo "> Checking internet connection"
-# ping -c 1 8.8.8.8 > /dev/null 2>&1
-#
-# if [ $? -eq 0 ]; then
-#   echo "> Internet is connected."
-# else
-#   echo "> No internet connection. Exiting setup..."
-#   exit 1
-# fi
+echo "> Checking internet connection"
+ping -c 1 8.8.8.8 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "> Internet is connected."
+else
+  echo "> No internet connection. Exiting installation..."
+  exit 1
+fi
 
 # prompt for installation process confirmation
 echo "You are about to install the flin dotfiles. Continue at your own risk."
