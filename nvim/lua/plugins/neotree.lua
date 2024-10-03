08af15cc60873030355b9dfb6f134cd7d0896104
Frederik Lind <frederik.lind.mail@gmail.com>
2024-10-03 12:39:45 +0200

    neotree diagnostics glyphs

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,51 +1,50 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
 
-    config = function()
-        require("neo-tree").setup({
-            window = {
-                position = "left",
-                width = 35,
-            },
-            enable_git_status = true,
-            git_status = {
-                symbols = {
-                    -- Change type
-                    added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                    modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                    deleted = "✖ ", -- this can only be used in the git_status source
-                    renamed = "󰁕 ", -- this can only be used in the git_status source
-                    -- Status type
-                    untracked = "",
-                    ignored = "", --"",
-                    unstaged = "󰄱 ",
-                    staged = " ",
-                    conflict = " ",
-                },
-            },
-
-            filesystem = {
-                filtered_items = {
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                },
-            },
-            icon = {
-                folder_closed = "",
-                folder_open = "",
-                folder_empty = "",
-                default = "",
-                highlight = "NeoTreeFileIcon",
-            },
-            modified = {
-                symbol = "",
-                highlight = "NeoTreeModified",
-            },
-        })
-    end,
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				position = "left",
+				width = 35,
+			},
+			enable_git_status = true,
+			git_status = {
+				symbols = {
+					-- Change type
+					added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+					modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+					deleted = "✖ ", -- this can only be used in the git_status source
+					renamed = "󰁕 ", -- this can only be used in the git_status source
+					-- Status type
+					untracked = "",
+					ignored = "", --"",
+					unstaged = "󰄱 ",
+					staged = " ",
+					conflict = " ",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+			icon = {
+				folder_closed = "",
+				folder_open = "",
+				folder_empty = "",
+				default = "",
+				highlight = "NeoTreeFileIcon",
+			},
+			modified = {
+				symbol = "",
+				highlight = "NeoTreeModified",
+			},
+		})
+	end,
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,6 +1,6 @@
 vim.cmd("let g:loaded_perl_provider = 0")
 vim.cmd("let g:loaded_ruby_provider = 0")
-vim.cmd("let g:python3_host_prog = \'/usr/bin/python3\'")
+vim.cmd("let g:python3_host_prog = '/usr/bin/python3'")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -17,77 +17,219 @@ vim.opt.wrap = false
 vim.o.pumheight = 15
 vim.g.mapleader = " "
 
-vim.keymap.set({'n', 'i'}, '<C-s>', ':w<CR>', { noremap = true, silent = false, desc = ":w<CR> - Writes changes to the current active file" })
-vim.keymap.set('n', '<C-q>', ':q<CR>', { noremap = true, silent = false, desc = ":q<CR> - Quits the current active file" })
-vim.keymap.set('n', '<leader>wq', ':wqa<CR>', { noremap = true, silent = false, desc = ":wqa<CR> - Writes changes to all files and quits." })
-vim.keymap.set('n', '<leader>wr', ':set wrap! linebreak!<CR>', { noremap = true, silent = true, desc = ":set wrap!<CR> - Toggles text wrapping." })
+vim.keymap.set(
+    { "n", "i" },
+    "<C-s>",
+    ":w<CR>",
+    { noremap = true, silent = false, desc = ":w<CR> - Writes changes to the current active file" }
+)
+vim.keymap.set(
+    "n",
+    "<C-q>",
+    ":q<CR>",
+    { noremap = true, silent = false, desc = ":q<CR> - Quits the current active file" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>wq",
+    ":wqa<CR>",
+    { noremap = true, silent = false, desc = ":wqa<CR> - Writes changes to all files and quits." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>wr",
+    ":set wrap! linebreak!<CR>",
+    { noremap = true, silent = true, desc = ":set wrap!<CR> - Toggles text wrapping." }
+)
 
 -- awesomeness right here
-vim.keymap.set('x', '<leader>p', '\"_dP)', { noremap = true, silent = true, desc = "Pastes without yanking. (when replacing in visual mode)" })
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { noremap = true, silent = true, desc = "Search and replace word under cursor." })
+vim.keymap.set(
+    "x",
+    "<leader>p",
+    '"_dP)',
+    { noremap = true, silent = true, desc = "Pastes without yanking. (when replacing in visual mode)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>s",
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { noremap = true, silent = true, desc = "Search and replace word under cursor." }
+)
 
 -- ============================ windows, buffers and tabs ===========================
 --tabline
-vim.keymap.set('n', '<F2>', ':set showtabline=2<CR>', { noremap = true, silent = true, desc = ":set showtabline=2<CR> - Shows the tabline." })
-vim.keymap.set('n', '<F3>', ':set showtabline=0<CR>', { noremap = true, silent = true, desc = ":set showtabline=0<CR> - Hides the tabline." })
+vim.keymap.set(
+    "n",
+    "<F2>",
+    ":set showtabline=2<CR>",
+    { noremap = true, silent = true, desc = ":set showtabline=2<CR> - Shows the tabline." }
+)
+vim.keymap.set(
+    "n",
+    "<F3>",
+    ":set showtabline=0<CR>",
+    { noremap = true, silent = true, desc = ":set showtabline=0<CR> - Hides the tabline." }
+)
 
 --tabs
-vim.keymap.set('n', '<S-Tab>', ':tabnext<CR>', { noremap = true, silent = true, desc = ":tabnext<CR> - Navigate to next tab." })
-vim.keymap.set('n', '<leader>tn', ':tabnew | Alpha<CR>', { noremap = true, silent = true, desc = ":tabnew<CR> - Creates a new tab." })
-vim.keymap.set('n', '<leader>tq', ':tabclose<CR>', { noremap = true, silent = true, desc = ":tabclose<CR> - Closes the current active tab." })
+vim.keymap.set(
+    "n",
+    "<S-Tab>",
+    ":tabnext<CR>",
+    { noremap = true, silent = true, desc = ":tabnext<CR> - Navigate to next tab." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>tn",
+    ":tabnew | Alpha<CR>",
+    { noremap = true, silent = true, desc = ":tabnew<CR> - Creates a new tab." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>tq",
+    ":tabclose<CR>",
+    { noremap = true, silent = true, desc = ":tabclose<CR> - Closes the current active tab." }
+)
 
 -- windows
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { silent = true, desc = ":wincmd k<CR> - Move to window above." })
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', { silent = true, desc = ":wincmd j<CR> - Move to window below." })
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { silent = true, desc = ":wincmd h<CR> - Move to window on the left." })
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { silent = true, desc = ":wincmd l<CR> - Move to window on the right." })
-vim.keymap.set('n', '<S-Up', ':resize +2<CR>', { noremap = true, silent = true, desc = ":resize +2<CR> - Vertical resize current window." })
-vim.keymap.set('n', '<S-Down>', ':resize -2<CR>', { noremap = true, silent = true, desc = ":resize -2<CR> - Vertical resize current window." })
-vim.keymap.set('n', '<S-Left>', ':vertical resize +2<CR>', { noremap = true, silent = true, desc = ":vertical resize +2<CR> - Horizontal resize current window." })
-vim.keymap.set('n', '<S-Right>', ':vertical resize -2<CR>', { noremap = true, silent = true, desc = ":vertical resize -2<CR> - Horizontal resize current window." })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true, desc = ":wincmd k<CR> - Move to window above." })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true, desc = ":wincmd j<CR> - Move to window below." })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true, desc = ":wincmd h<CR> - Move to window on the left." })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true, desc = ":wincmd l<CR> - Move to window on the right." })
+vim.keymap.set(
+    "n",
+    "<S-Up",
+    ":resize +2<CR>",
+    { noremap = true, silent = true, desc = ":resize +2<CR> - Vertical resize current window." }
+)
+vim.keymap.set(
+    "n",
+    "<S-Down>",
+    ":resize -2<CR>",
+    { noremap = true, silent = true, desc = ":resize -2<CR> - Vertical resize current window." }
+)
+vim.keymap.set(
+    "n",
+    "<S-Left>",
+    ":vertical resize +2<CR>",
+    { noremap = true, silent = true, desc = ":vertical resize +2<CR> - Horizontal resize current window." }
+)
+vim.keymap.set(
+    "n",
+    "<S-Right>",
+    ":vertical resize -2<CR>",
+    { noremap = true, silent = true, desc = ":vertical resize -2<CR> - Horizontal resize current window." }
+)
 
--- buffers 
-vim.keymap.set('n', '<leader>p', ':bprev<CR>', { noremap = true, silent = true, desc = ":bprev<CR> - Navigate to previous buffer." })
-vim.keymap.set('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true, desc = ":bnext<CR> - Navigate to next buffer." })
-vim.keymap.set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true, desc = ":bd<CR> - Close current buffer." })
-vim.keymap.set('n', '<leader>bp', ':bp<CR>', { noremap = true, silent = true, desc = ":bp<CR> - Previous buffer." })
+-- buffers
+vim.keymap.set(
+    "n",
+    "<leader>p",
+    ":bprev<CR>",
+    { noremap = true, silent = true, desc = ":bprev<CR> - Navigate to previous buffer." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>n",
+    ":bnext<CR>",
+    { noremap = true, silent = true, desc = ":bnext<CR> - Navigate to next buffer." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>bd",
+    ":bd<CR>",
+    { noremap = true, silent = true, desc = ":bd<CR> - Close current buffer." }
+)
+vim.keymap.set("n", "<leader>bp", ":bp<CR>", { noremap = true, silent = true, desc = ":bp<CR> - Previous buffer." })
 
 -- telescope
-vim.keymap.set('n', '<leader>cf', ':Telescope commands<CR>', { noremap = true, silent = false, desc = ":Telescope commands<CR> - Fuzzy finder for commands." })
-vim.keymap.set('n', '<S-f>', ':Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true, desc = "Fuzzy finder within current buffer."})
-vim.keymap.set('n', '<leader>bf', ':Telescope buffers<CR>', { noremap = true, silent = true, desc = ":Telescope buffers<CR> - Fuzzy finder for buffers." })
+vim.keymap.set(
+    "n",
+    "<leader>cf",
+    ":Telescope commands<CR>",
+    { noremap = true, silent = false, desc = ":Telescope commands<CR> - Fuzzy finder for commands." }
+)
+vim.keymap.set(
+    "n",
+    "<S-f>",
+    ":Telescope current_buffer_fuzzy_find<CR>",
+    { noremap = true, silent = true, desc = "Fuzzy finder within current buffer." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>bf",
+    ":Telescope buffers<CR>",
+    { noremap = true, silent = true, desc = ":Telescope buffers<CR> - Fuzzy finder for buffers." }
+)
 
 -- neo-tree
-vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = ":Neotree toggle<CR> - Toggles visibility of neo-tree." })
-vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>', { noremap = true, silent = true, desc = ":Neotree focus<CR> - Jump cursor to neo-tree." })
+vim.keymap.set(
+    "n",
+    "<C-n>",
+    ":Neotree toggle<CR>",
+    { noremap = true, silent = true, desc = ":Neotree toggle<CR> - Toggles visibility of neo-tree." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>e",
+    ":Neotree focus<CR>",
+    { noremap = true, silent = true, desc = ":Neotree focus<CR> - Jump cursor to neo-tree." }
+)
 
 -- neogit & git tree
-vim.keymap.set('n', '<leader>gs', function()
-  vim.cmd('Neogit')
-  vim.cmd('Neotree git_status')
-  vim.cmd('wincmd l')
+vim.keymap.set("n", "<leader>gs", function()
+    vim.cmd("Neogit")
+    vim.cmd("Neotree git_status")
+    vim.cmd("wincmd l")
 end, { noremap = true, silent = true, desc = "Opens Neogit, for managing git changes and commits." })
 
 --git (diffview)
-vim.keymap.set('n', '<leader>fh', ':DiffviewFileHistory<CR>', { noremap = true, silent = true, desc = ":DiffviewFileHistory<CR> - Opens file history in diffview." })
-vim.keymap.set('n', '<leader>df', ':DiffviewFileHistory %<CR>', { noremap = true, silent = true, desc = ":DiffviewFileHistory %<CR> - Opens file history in diffview for current file." })
+vim.keymap.set(
+    "n",
+    "<leader>fh",
+    ":DiffviewFileHistory<CR>",
+    { noremap = true, silent = true, desc = ":DiffviewFileHistory<CR> - Opens file history in diffview." }
+)
+vim.keymap.set(
+    "n",
+    "<leader>df",
+    ":DiffviewFileHistory %<CR>",
+    {
+        noremap = true,
+        silent = true,
+        desc = ":DiffviewFileHistory %<CR> - Opens file history in diffview for current file.",
+    }
+)
 
 --launch lazy/mason
-vim.keymap.set('n', '<leader>lz', ':Lazy<CR>', { noremap = true, silent = true, desc = ":Lazy<CR> - Launches lazy." })
-vim.keymap.set('n', '<leader>mn', ':Mason<CR>', { noremap = true, silent = true, desc = ":Mason<CR> - Launches mason." })
+vim.keymap.set("n", "<leader>lz", ":Lazy<CR>", { noremap = true, silent = true, desc = ":Lazy<CR> - Launches lazy." })
+vim.keymap.set(
+    "n",
+    "<leader>mn",
+    ":Mason<CR>",
+    { noremap = true, silent = true, desc = ":Mason<CR> - Launches mason." }
+)
 
-
-vim.keymap.set('n', '<leader>bt', ':Barbecue toggle<CR>', { noremap = true, silent = true, desc = ":Barbecue toggle<CR> - Toggles barbecue." })
+vim.keymap.set(
+    "n",
+    "<leader>bt",
+    ":Barbecue toggle<CR>",
+    { noremap = true, silent = true, desc = ":Barbecue toggle<CR> - Toggles barbecue." }
+)
 
 -- =================================== Diagnostics ===================================
 
--- vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
--- vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
--- vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
--- vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
--- vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
--- vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+vim.diagnostic.config({
+    signs = {
+        --support diagnostic severity / diagnostic type name
+        text = {
+            [1] = "",
+            ["WARN"] = "",
+            ["HINT"] = "",
+        },
+    },
+})
 
 vim.filetype.add({
-  pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+    pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
