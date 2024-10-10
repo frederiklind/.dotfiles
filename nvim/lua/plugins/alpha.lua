dec788e215965c18c8c757d5401a9920c633a884
Frederik Lind <frederik.lind.mail@gmail.com>
2024-10-10 10:20:42 +0200

    changes

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,70 +1,176 @@
-local function configure()
-    local dashboard = require('alpha.themes.dashboard')
-
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#8aadf4" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#7dc4e4" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#91d7e3" })
-    vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#8bd5ca" })
-
-    dashboard.section.header.type = "group"
-    dashboard.section.header.val = {
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-
-
-
-        { type = "text", val = "                                                                     ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-        { type = "text", val = "       ████ ██████           █████      ██                     ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-        { type = "text", val = "      ███████████             █████                             ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-        { type = "text", val = "      █████████ ███████████████████ ███   ███████████   ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-        { type = "text", val = "     █████████  ███    █████████████ █████ ██████████████   ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-        { type = "text", val = "    █████████ ██████████ █████████ █████ █████ ████ █████   ", opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" } },
-        { type = "text", val = "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ", opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" } },
-        { type = "text", val = " ██████  █████████████████████ ████ █████ █████ ████ ██████ ", opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" } },
-
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
-
-
-	}
-
-    dashboard.section.buttons.val = {
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
-        dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-    }
-
-    return dashboard.config
-end
+-- local function configure()
+--     local dashboard = require('alpha.themes.dashboard')
+--
+--     vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#8aadf4" })
+--     vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#7dc4e4" })
+--     vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#91d7e3" })
+--     vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#8bd5ca" })
+--
+--     dashboard.section.header.type = "group"
+--     dashboard.section.header.val = {
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--
+--
+--
+--         { type = "text", val = "                                                                     ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "       ████ ██████           █████      ██                     ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "      ███████████             █████                             ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "      █████████ ███████████████████ ███   ███████████   ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "     █████████  ███    █████████████ █████ ██████████████   ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "    █████████ ██████████ █████████ █████ █████ ████ █████   ", opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" } },
+--         { type = "text", val = "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ", opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" } },
+--         { type = "text", val = " ██████  █████████████████████ ████ █████ █████ ████ ██████ ", opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" } },
+--
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+-- 		{ type = "text", val = "                                                                      ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+--
+--
+-- 	}
+--
+    -- dashboard.section.buttons.val = {
+    --     dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+    --     dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+    --     dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+    --     dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
+    --     dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+    --     dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+    --     dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+    --     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    -- }
+--
+--     return dashboard.config
+-- end
+--
+-- return {
+--     "goolord/alpha-nvim",
+--     lazy = true,
+--     event = "VimEnter",
+--     config = function(_, dashboard)
+--         if vim.o.filetype == "lazy" then
+--             vim.cmd.close()
+--             vim.api.nvim_create_autocmd("User", {
+--                 pattern = "AlphaReady",
+--                 callback = function()
+--                     require("lazy").show()
+--                 end,
+--             })
+--         end
+--
+--         require('alpha').setup(configure())
+--     end,
+-- }
 
 return {
     "goolord/alpha-nvim",
-    lazy = true,
-    event = "VimEnter",
-    config = function(_, dashboard)
-        if vim.o.filetype == "lazy" then
-            vim.cmd.close()
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "AlphaReady",
-                callback = function()
-                    require("lazy").show()
-                end,
-            })
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local dashboard = require("alpha.themes.dashboard")
+
+        -- helper function for utf8 chars
+        local function getCharLen(s, pos)
+            local byte = string.byte(s, pos)
+            if not byte then
+                return nil
+            end
+            return (byte < 0x80 and 1) or (byte < 0xE0 and 2) or (byte < 0xF0 and 3) or (byte < 0xF8 and 4) or 1
         end
 
-        require('alpha').setup(configure())
+        local function applyColors(logo, colors, logoColors)
+            dashboard.section.header.val = logo
+
+            for key, color in pairs(colors) do
+                local name = "Alpha" .. key
+                vim.api.nvim_set_hl(0, name, color)
+                colors[key] = name
+            end
+
+            dashboard.section.header.opts.hl = {}
+            for i = 1, #logo do
+                local line = logo[i]
+                local colorLine = logoColors[i] or ""
+                local highlights = {}
+                local pos = 0
+
+                for j = 1, #colorLine do
+                    local opos = pos
+                    pos = pos + getCharLen(line, opos + 1)
+
+                    local color_name = colors[colorLine:sub(j, j)]
+                    if color_name then
+                        table.insert(highlights, { color_name, opos, pos })
+                    end
+                end
+
+                table.insert(dashboard.section.header.opts.hl, highlights)
+            end
+            return dashboard.opts
+        end
+
+        local logo = {
+            [[                                                                   ]],
+            [[      ███████████           █████      ██                    ]],
+            [[     ███████████             █████                            ]],
+            [[     ████████████████ ███████████ ███   ███████████  ]],
+            [[    ████████████████ ████████████ █████ ██████████████  ]],
+            [[   █████████████████████████████ █████ █████ ████ █████  ]],
+            [[ ██████████████████████████████████ █████ █████ ████ █████ ]],
+            [[██████  ███ █████████████████ ████ █████ █████ ████ ██████]],
+            [[██████   ██  ███████████████   ██ █████████████████]]
+        }
+
+        local logoColors = {
+            [[                                             bb                      ]],
+            [[      ccccccccccccccc           ccccccc      bbbb                    ]],
+            [[     ccccccccccccccc             ccccccc c                           ]],
+            [[     cccccccccccccabbbbbbbbb bbbbbcccccccccc bbbb   bbbbbbbbbbbbbbb  ]],
+            [[    cccccccccccccbbbbbbbbbbb bbbbbbccccccccc bbbbb bbbbbbbbbbbbbbbb  ]],
+            [[   cccccccccccccbbbbbbbddddbbbbbbbbbcccccccc bbbbb bbbbb bbbb bbbbb  ]],
+            [[ cccccccccccccccbbbbbbbbbbbbbbbbbbbbbccccccc bbbbb bbbbb bbbb bbbbbb ]],
+            [[cccccccc  ccccc bbbbbbbbbbbbbbbbbbbbb cccccc bbbbb bbbbb bbbb bbbbbbb]],
+            [[aaaaaaaa   aaaa  ddddddddddddddddddd   aaaa ddddddddddddddddddddddddd]]
+        }
+
+        dashboard.section.buttons.val = {
+            dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+            dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
+            dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+            dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+            dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+            dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+        }
+
+        require("alpha").setup({
+            layout = {
+                { type = "padding", val = 10 },  -- Adds vertical padding before the ASCII art
+                dashboard.section.header,
+                { type = "padding", val = 6 },  -- Additional padding between the ASCII art and buttons
+                dashboard.section.buttons,
+            },
+            opts = applyColors(logo, {
+                ["b"] = { fg = "#8aadf4", ctermfg = 33 },
+                ["d"] = { fg = "#4e83ef", ctermfg = 33 },
+                ["c"] = { fg = "#8bd5ca", ctermfg = 33 },
+                ["a"] = { fg = "#3aa192", ctermfg = 35 },
+                ["g"] = { fg = "#91d7e3", ctermfg = 29 },
+                ["h"] = { fg = "#8bd5ca", ctermfg = 23 },
+                ["i"] = { fg = "#a6da95", bg = "#39ac56", ctermfg = 23, ctermbg = 29 },
+                ["j"] = { fg = "#b7bdf8", bg = "#33994d", ctermfg = 35, ctermbg = 23 },
+                ["k"] = { fg = "#30A572", ctermfg = 36 },
+            }, logoColors)
+        })
     end,
 }
