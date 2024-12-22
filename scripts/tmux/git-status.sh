801ae623b839a5136c8277af0386ab81c8a99f46
Frederik Lind <frederik.lind.mail@gmail.com>
2024-12-22 15:10:22 +0100

    changes

--- scripts/tmux/git-status.sh
+++ scripts/tmux/git-status.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    # Get the current branch name
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    # Output the Git icon and branch name
     echo " ${BRANCH}"
 else
     echo ""
