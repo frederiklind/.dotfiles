54cb8c0d6d85afa0cc5e8943d36dd644273e55f9
frederiklind <frederik.lind.mail@proton.me>
2025-03-10 02:23:24 +0100

    tmux

--- tmux/batt.sh
+++ tmux/batt.sh
@@ -3,19 +3,11 @@
 if [ -f /sys/class/power_supply/BAT0/capacity ]; then
     batt_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
     
-    if [ "$batt_lvl" -ge 80 ]; then
-        icon=""
-    elif [ "$batt_lvl" -ge 60 ]; then
-        icon=""
-    elif [ "$batt_lvl" -ge 40 ]; then
-        icon=""
-    elif [ "$batt_lvl" -ge 20 ]; then
-        icon=""
+    if [ "$batt_lvl" -le 30 ]; then
+        echo "#[fg=#fab387,bg=#181926]󱐋"
     else
-        icon=""
-    fi
-
-    echo "  #[fg=#74c7ec,bg=#181926]$icon "
+        echo ""
+    fi 
 else
     echo ""
 fi
--- tmux/git.sh
+++ tmux/git.sh
@@ -5,7 +5,7 @@ pane_cwd=$(tmux display-message -p '#{pane_current_path}')
 
 # Check if the active pane's CWD is a Git repository root
 if [[ "$(git -C "$pane_cwd" rev-parse --show-toplevel 2>/dev/null)" == "$pane_cwd" ]]; then
-    echo "#[fg=#74c7ec]"
+    echo "#[fg=#74c7ec] "
 else
     echo ""
 fi
