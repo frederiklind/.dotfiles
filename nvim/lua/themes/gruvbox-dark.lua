59e9898151eaad2c37a6080cafdc1adf4c145d16
frederiklind <frederik.lind.mail@proton.me>
2025-03-12 11:29:57 +0100

    changes

--- nvim/lua/themes/gruvbox-dark.lua
+++ nvim/lua/themes/gruvbox-dark.lua
@@ -7,3 +7,15 @@ return {
         vim.cmd.colorscheme("gruvbox-material")
     end,
 }
+
+
+-- return { 
+--     "ellisonleao/gruvbox.nvim", 
+--     priority = 1000 , 
+--     config = function()
+--         require("gruvbox").setup({
+--             transparent_mode = true,
+--         })
+--         vim.cmd.colorscheme("gruvbox")
+--     end,
+-- }
